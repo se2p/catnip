6eb955ae85960057c043fc77aadcde140159d14a
obermuel <obermuel@fim.uni-passau.de>
2020-07-13 13:13:08 +0200

    add edit class to contain siblings of the edit, to get a better understanding where it has to be placed

---
+++ src/main/java/de/uni_passau/fim/se2/catnip/pqGram/EditSet.java
@@ -0,0 +1,43 @@
+package de.uni_passau.fim.se2.catnip.pqGram;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class EditSet {
+    private Set<Edit> additions;
+    private Set<Edit> deletions;
+
+    public EditSet() {
+        additions = new LinkedHashSet<>();
+        deletions = new LinkedHashSet<>();
+    }
+
+    public EditSet(Set<Edit> additions, Set<Edit> deletions) {
+        this.additions = new LinkedHashSet<>(additions);
+        this.deletions = new LinkedHashSet<>(deletions);
+    }
+
+    public void addAddition(Edit edit) {
+        additions.add(edit);
+    }
+
+    public void addDeletion(Edit deletion) {
+        deletions.add(deletion);
+    }
+
+    public Set<Edit> getAdditions() {
+        return additions;
+    }
+
+    public Set<Edit> getDeletions() {
+        return deletions;
+    }
+
+    @Override
+    public String toString() {
+        return "Edits{" +
+                "additions=" + additions +
+                ", deletions=" + deletions +
+                '}';
+    }
+}
