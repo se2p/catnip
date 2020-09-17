7639fe9819613f153e7e6c66dd9dbca70331408b
Florian Obermueller <obermuel@fim.uni-passau.de>
2020-09-17 11:28:20 +0200

    make tests nicer

--- src/test/java/recommendation/EditsGeneratorTest.java
+++ src/test/java/recommendation/EditsGeneratorTest.java
@@ -1,12 +1,8 @@
 package recommendation;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import de.uni_passau.fim.se2.catnip.recommendation.Edit;
-import de.uni_passau.fim.se2.catnip.recommendation.EditSet;
 import de.uni_passau.fim.se2.catnip.pq_gram.Label;
-import de.uni_passau.fim.se2.catnip.recommendation.ActorBlockEdit;
-import de.uni_passau.fim.se2.catnip.recommendation.ActorScriptEdit;
-import de.uni_passau.fim.se2.catnip.recommendation.EditsGenerator;
+import de.uni_passau.fim.se2.catnip.recommendation.*;
 import de.uni_passau.fim.se2.litterbox.ast.ParsingException;
 import de.uni_passau.fim.se2.litterbox.ast.model.Program;
 import de.uni_passau.fim.se2.litterbox.ast.parser.ProgramParser;
@@ -23,57 +19,24 @@ import java.util.List;
 import java.util.Set;
 
 public class EditsGeneratorTest {
-    private static Program oneBlockDifferenceSource;
-    private static Program oneBlockDifferenceTarget;
-    private static Program sameBlocksSource;
-    private static Program sameBlocksTarget;
     private static Program empty;
-    private static Program oneScript;
-    private static Program oneProcedureSource;
-    private static Program oneProcedureTarget;
-    private static Program tooMuchSource;
-    private static Program tooMuchTarget;
-    private static Program tooMuchScriptSource;
-    private static Program tooMuchScriptTarget;
     private static Program deadNewScript;
-    private static Program deadNewScriptWithExp;
-    private static ObjectMapper mapper = new ObjectMapper();
+    private static final ObjectMapper mapper = new ObjectMapper();
 
     @BeforeAll
     public static void setUp() throws IOException, ParsingException {
-
-        File f = new File("./src/test/fixtures/oneBlockDifferenceSource.json");
-        oneBlockDifferenceSource = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
-        f = new File("./src/test/fixtures/oneBlockDifferenceTarget.json");
-        oneBlockDifferenceTarget = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
-        f = new File("./src/test/fixtures/sameBlocksSource.json");
-        sameBlocksSource = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
-        f = new File("./src/test/fixtures/sameBlocksTarget.json");
-        sameBlocksTarget = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
-        f = new File("./src/test/fixtures/emptyProject.json");
+        File f = new File("./src/test/fixtures/emptyProject.json");
         empty = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
-        f = new File("./src/test/fixtures/oneScript.json");
-        oneScript = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
-        f = new File("./src/test/fixtures/oneProcedureSource.json");
-        oneProcedureSource = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
-        f = new File("./src/test/fixtures/oneProcedureTarget.json");
-        oneProcedureTarget = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
-        f = new File("./src/test/fixtures/tooMuchTarget.json");
-        tooMuchTarget = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
-        f = new File("./src/test/fixtures/tooMuchSource.json");
-        tooMuchSource = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
-        f = new File("./src/test/fixtures/tooMuchScriptTarget.json");
-        tooMuchScriptTarget = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
-        f = new File("./src/test/fixtures/tooMuchScriptSource.json");
-        tooMuchScriptSource = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
         f = new File("./src/test/fixtures/deadNewScript.json");
         deadNewScript = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
-        f = new File("./src/test/fixtures/deadNewScriptWithExp.json");
-        deadNewScriptWithExp = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
     }
 
     @Test
-    public void testOneBlockDifference() {
+    public void testOneBlockDifference() throws IOException, ParsingException {
+        File f = new File("./src/test/fixtures/oneBlockDifferenceSource.json");
+        Program oneBlockDifferenceSource = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
+        f = new File("./src/test/fixtures/oneBlockDifferenceTarget.json");
+        Program oneBlockDifferenceTarget = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
         List<Program> targets = new ArrayList<>();
         targets.add(oneBlockDifferenceTarget);
         EditsGenerator editsGenerator = new EditsGenerator(oneBlockDifferenceSource, targets);
@@ -86,34 +49,37 @@ public class EditsGeneratorTest {
         Assertions.assertEquals(0, edit.getDeletions().size());
         Assertions.assertEquals(4, edit.getAdditions().size());
         Edit addition1 = new Edit(new Label("StmtList", null), new Label("IfOnEdgeBounce", null));
-        Label goTo = new Label("GoToPos",null);
-        Label emptyLabel = new Label("*",null);
+        Label goTo = new Label("GoToPos", null);
+        Label emptyLabel = new Label("*", null);
         List<Label> left = new ArrayList<>();
         List<Label> right = new ArrayList<>();
         right.add(goTo);
         right.add(emptyLabel);
-        Edit addition2 = new Edit(new Label("StmtList", null), new Label("IfOnEdgeBounce", null),new ArrayList<>(left),new ArrayList<>(right));
+        Edit addition2 = new Edit(new Label("StmtList", null), new Label("IfOnEdgeBounce", null), new ArrayList<>(left), new ArrayList<>(right));
         left = new ArrayList<>();
         right = new ArrayList<>();
         left.add(emptyLabel);
         left.add(emptyLabel);
-        Edit addition3 = new Edit(new Label("StmtList", null), new Label("IfOnEdgeBounce", null),new ArrayList<>(left),new ArrayList<>(right));
+        Edit addition3 = new Edit(new Label("StmtList", null), new Label("IfOnEdgeBounce", null), new ArrayList<>(left), new ArrayList<>(right));
         left = new ArrayList<>();
         right = new ArrayList<>();
         left.add(emptyLabel);
         right.add(goTo);
-        Edit addition4 = new Edit(new Label("StmtList", null), new Label("IfOnEdgeBounce", null),new ArrayList<>(left),new ArrayList<>(right));
+        Edit addition4 = new Edit(new Label("StmtList", null), new Label("IfOnEdgeBounce", null), new ArrayList<>(left), new ArrayList<>(right));
         Set<Edit> additions = new LinkedHashSet<>();
         additions.add(addition1);
         additions.add(addition2);
         additions.add(addition3);
         additions.add(addition4);
-        System.out.println(edit.getAdditions());
         Assertions.assertEquals(additions, edit.getAdditions());
     }
 
     @Test
-    public void testSameBlocksDifference() {
+    public void testSameBlocksDifference() throws IOException, ParsingException {
+        File f = new File("./src/test/fixtures/sameBlocksSource.json");
+        Program sameBlocksSource = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
+        f = new File("./src/test/fixtures/sameBlocksTarget.json");
+        Program sameBlocksTarget = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
         List<Program> targets = new ArrayList<>();
         targets.add(sameBlocksTarget);
         EditsGenerator editsGenerator = new EditsGenerator(sameBlocksSource, targets);
@@ -125,24 +91,24 @@ public class EditsGeneratorTest {
         EditSet edit = actorEdit.getEdit();
         Assertions.assertEquals(0, edit.getDeletions().size());
         Assertions.assertEquals(4, edit.getAdditions().size());
-        Label ifOnEdge = new Label("IfOnEdgeBounce",null);
-        Label emptyLabel = new Label("*",null);
+        Label ifOnEdge = new Label("IfOnEdgeBounce", null);
+        Label emptyLabel = new Label("*", null);
         List<Label> left = new ArrayList<>();
         List<Label> right = new ArrayList<>();
         left.add(emptyLabel);
         left.add(ifOnEdge);
         Edit addition1 = new Edit(new Label("StmtList", null), new Label("ClearGraphicEffects", null));
-        Edit addition2 = new Edit(new Label("StmtList", null), new Label("ClearGraphicEffects", null),new ArrayList<>(left),new ArrayList<>(right));
+        Edit addition2 = new Edit(new Label("StmtList", null), new Label("ClearGraphicEffects", null), new ArrayList<>(left), new ArrayList<>(right));
         left = new ArrayList<>();
         right = new ArrayList<>();
         right.add(emptyLabel);
         right.add(emptyLabel);
-        Edit addition3 = new Edit(new Label("StmtList", null), new Label("ClearGraphicEffects", null),new ArrayList<>(left),new ArrayList<>(right));
+        Edit addition3 = new Edit(new Label("StmtList", null), new Label("ClearGraphicEffects", null), new ArrayList<>(left), new ArrayList<>(right));
         left = new ArrayList<>();
         right = new ArrayList<>();
         left.add(ifOnEdge);
         right.add(emptyLabel);
-        Edit addition4 = new Edit(new Label("StmtList", null), new Label("ClearGraphicEffects", null),new ArrayList<>(left),new ArrayList<>(right));
+        Edit addition4 = new Edit(new Label("StmtList", null), new Label("ClearGraphicEffects", null), new ArrayList<>(left), new ArrayList<>(right));
         Set<Edit> additions = new LinkedHashSet<>();
         additions.add(addition1);
         additions.add(addition2);
@@ -152,13 +118,15 @@ public class EditsGeneratorTest {
     }
 
     @Test
-    public void testOneScriptDifference() {
+    public void testOneScriptDifference() throws IOException, ParsingException {
+        File f = new File("./src/test/fixtures/oneScript.json");
+        Program oneScript = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
         List<Program> targets = new ArrayList<>();
         targets.add(oneScript);
         EditsGenerator editsGenerator = new EditsGenerator(empty, targets);
         List<ActorBlockEdit> actorEdits = editsGenerator.getEdits();
         Assertions.assertEquals(1, actorEdits.size());
-        ActorBlockEdit actorEdit =  actorEdits.get(0);
+        ActorBlockEdit actorEdit = actorEdits.get(0);
         Assertions.assertEquals("Figur1", actorEdit.getActor().getIdent().getName());
         EditSet edit = actorEdit.getEdit();
         Assertions.assertEquals(0, edit.getDeletions().size());
@@ -170,13 +138,17 @@ public class EditsGeneratorTest {
     }
 
     @Test
-    public void testTooMuch() {
+    public void testTooMuch() throws IOException, ParsingException {
+        File f = new File("./src/test/fixtures/tooMuchTarget.json");
+        Program tooMuchTarget = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
+        f = new File("./src/test/fixtures/tooMuchSource.json");
+        Program tooMuchSource = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
         List<Program> targets = new ArrayList<>();
         targets.add(tooMuchTarget);
         EditsGenerator editsGenerator = new EditsGenerator(tooMuchSource, targets);
         List<ActorBlockEdit> actorEdits = editsGenerator.getEdits();
         Assertions.assertEquals(1, actorEdits.size());
-        ActorBlockEdit actorEdit =  actorEdits.get(0);
+        ActorBlockEdit actorEdit = actorEdits.get(0);
         Assertions.assertEquals("Figur1", actorEdit.getActor().getIdent().getName());
         EditSet edit = actorEdit.getEdit();
         Assertions.assertEquals(8, edit.getDeletions().size());
@@ -189,13 +161,17 @@ public class EditsGeneratorTest {
     }
 
     @Test
-    public void testProcedureDifference() {
+    public void testProcedureDifference() throws IOException, ParsingException {
+        File f = new File("./src/test/fixtures/oneProcedureSource.json");
+        Program oneProcedureSource = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
+        f = new File("./src/test/fixtures/oneProcedureTarget.json");
+        Program oneProcedureTarget = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
         List<Program> targets = new ArrayList<>();
         targets.add(oneProcedureTarget);
         EditsGenerator editsGenerator = new EditsGenerator(oneProcedureSource, targets);
         List<ActorBlockEdit> actorEdits = editsGenerator.getEdits();
         Assertions.assertEquals(1, actorEdits.size());
-        ActorBlockEdit actorEdit =  actorEdits.get(0);
+        ActorBlockEdit actorEdit = actorEdits.get(0);
         Assertions.assertEquals("Figur1", actorEdit.getActor().getIdent().getName());
         EditSet edit = actorEdit.getEdit();
         Assertions.assertEquals(0, edit.getDeletions().size());
@@ -209,13 +185,17 @@ public class EditsGeneratorTest {
     }
 
     @Test
-    public void testTooSourceMuch() {
+    public void testTooSourceMuch() throws IOException, ParsingException {
+        File f = new File("./src/test/fixtures/tooMuchScriptTarget.json");
+        Program tooMuchScriptTarget = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
+        f = new File("./src/test/fixtures/tooMuchScriptSource.json");
+        Program tooMuchScriptSource = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
         List<Program> targets = new ArrayList<>();
         targets.add(tooMuchScriptTarget);
         EditsGenerator editsGenerator = new EditsGenerator(tooMuchScriptSource, targets);
         List<ActorBlockEdit> actorEdits = editsGenerator.getEdits();
         Assertions.assertEquals(1, actorEdits.size());
-        ActorBlockEdit actorEdit =  actorEdits.get(0);
+        ActorBlockEdit actorEdit = actorEdits.get(0);
         Assertions.assertEquals("Figur1", actorEdit.getActor().getIdent().getName());
         EditSet edit = actorEdit.getEdit();
         Assertions.assertEquals(1, edit.getDeletions().size());
@@ -232,7 +212,7 @@ public class EditsGeneratorTest {
         EditsGenerator editsGenerator = new EditsGenerator(empty, targets);
         List<ActorBlockEdit> actorEdits = editsGenerator.getEdits();
         Assertions.assertEquals(1, actorEdits.size());
-        ActorBlockEdit actorEdit =  actorEdits.get(0);
+        ActorBlockEdit actorEdit = actorEdits.get(0);
         Assertions.assertEquals("Figur1", actorEdit.getActor().getIdent().getName());
         EditSet edit = actorEdit.getEdit();
         Assertions.assertEquals(0, edit.getDeletions().size());
@@ -250,7 +230,7 @@ public class EditsGeneratorTest {
         EditsGenerator editsGenerator = new EditsGenerator(deadNewScript, targets);
         List<ActorBlockEdit> actorEdits = editsGenerator.getEdits();
         Assertions.assertEquals(1, actorEdits.size());
-        ActorBlockEdit actorEdit =  actorEdits.get(0);
+        ActorBlockEdit actorEdit = actorEdits.get(0);
         Assertions.assertEquals("Sprite1", actorEdit.getActor().getIdent().getName());
         EditSet edit = actorEdit.getEdit();
         Assertions.assertEquals(1, edit.getDeletions().size());
@@ -262,13 +242,15 @@ public class EditsGeneratorTest {
     }
 
     @Test
-    public void testExpressionAddition() {
+    public void testExpressionAddition() throws IOException, ParsingException {
+        File f = new File("./src/test/fixtures/deadNewScriptWithExp.json");
+        Program deadNewScriptWithExp = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
         List<Program> targets = new ArrayList<>();
         targets.add(deadNewScriptWithExp);
         EditsGenerator editsGenerator = new EditsGenerator(deadNewScript, targets);
         List<ActorBlockEdit> actorEdits = editsGenerator.getEdits();
         Assertions.assertEquals(1, actorEdits.size());
-        ActorBlockEdit actorEdit =  actorEdits.get(0);
+        ActorBlockEdit actorEdit = actorEdits.get(0);
         Assertions.assertEquals("Sprite1", actorEdit.getActor().getIdent().getName());
         EditSet edit = actorEdit.getEdit();
         Assertions.assertEquals(0, edit.getDeletions().size());
@@ -276,22 +258,22 @@ public class EditsGeneratorTest {
         Edit addition = new Edit(new Label("TurnRight", null), new Label("Size", null));
         Set<Edit> additions = new LinkedHashSet<>();
         additions.add(addition);
-        Label emptyLabel = new Label("*",null);
+        Label emptyLabel = new Label("*", null);
         List<Label> left = new ArrayList<>();
         List<Label> right = new ArrayList<>();
         left.add(emptyLabel);
         left.add(emptyLabel);
-        additions.add(new Edit(new Label("TurnRight", null), new Label("Size", null),left,right));
+        additions.add(new Edit(new Label("TurnRight", null), new Label("Size", null), left, right));
         left = new ArrayList<>();
         right = new ArrayList<>();
         left.add(emptyLabel);
-        right.add(new Label("TopNonDataBlockMetadata",null));
-        additions.add(new Edit(new Label("TurnRight", null), new Label("Size", null),left,right));
+        right.add(new Label("TopNonDataBlockMetadata", null));
+        additions.add(new Edit(new Label("TurnRight", null), new Label("Size", null), left, right));
         left = new ArrayList<>();
         right = new ArrayList<>();
-        right.add(new Label("TopNonDataBlockMetadata",null));
+        right.add(new Label("TopNonDataBlockMetadata", null));
         right.add(emptyLabel);
-        additions.add(new Edit(new Label("TurnRight", null), new Label("Size", null),left,right));
-        Assert.assertEquals(additions,edit.getAdditions());
+        additions.add(new Edit(new Label("TurnRight", null), new Label("Size", null), left, right));
+        Assert.assertEquals(additions, edit.getAdditions());
     }
 }
