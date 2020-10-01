60929931119873242e1765701e182eb546005c7f
obermuel <obermuel@fim.uni-passau.de>
2020-10-01 15:29:22 +0200

    adjust commandline handling

--- src/main/java/de/uni_passau/fim/se2/catnip/Main.java
+++ src/main/java/de/uni_passau/fim/se2/catnip/Main.java
@@ -3,27 +3,53 @@ package de.uni_passau.fim.se2.catnip;
 import org.apache.commons.cli.*;
 
 public class Main {
+    private static final String SOURCE_PATH = "path";
+    private static final String SOURCE_PATH_SHORT = "p";
+    private static final String TARGET_PATH = "target";
+    private static final String TARGET_PATH_SHORT = "t";
+    private static final String CSV_PATH = "csv";
+    private static final String CSV_PATH_SHORT = "c";
+    private static final String HELP = "help";
+    private static final String HELP_SHORT = "h";
 
-    public static void main(String[] args) throws ParseException {
-        Options options = new Options();
+    private Main() {
+    }
 
-        options.addOption("path", true, "file path (required)");
-        options.addOption("folder", true, "folder path to sample solutions and csv file" +
-                " (required)");
+    public static void main(String[] args) {
+        Options options = getCommandLineOptions();
 
         CommandLineParser parser = new DefaultParser();
+        try {
+            CommandLine cmd = parser.parse(options, args);
 
-        CommandLine cmd = parser.parse(options, args);
+            if (cmd.hasOption(SOURCE_PATH) && cmd.hasOption(TARGET_PATH) && cmd.hasOption(CSV_PATH)) {
 
-        if (cmd.hasOption("path") && cmd.hasOption("folder")) {
+                //TODO
 
-            //TODO
-
-            return;
+                return;
+            }
+            printHelp();
+        } catch (ParseException parseException) {
+            System.err.println("Invalid option: " + parseException.getMessage());
+            printHelp();
         }
+    }
+
+    static void printHelp() {
         HelpFormatter formatter = new HelpFormatter();
-        formatter.printHelp("Catnip", options);
-        System.out.println("Example: " + "java -cp C:\\Catnip1.0.jar de.uni_passau.fim.se2.catnip.Main -path " +
-                "C:\\scratchprojects\\files\\mySolution.sb3 -folder C:\\scratchprojects\\sampleSolutions");
+        formatter.printHelp("Catnip", getCommandLineOptions(), true);
+        System.out.println("Example: " + "java -jar Catnip.jar -path "
+                + "C:\\scratchprojects\\files\\mySolution.sb3 -target C:\\scratchprojects\\sampleSolutions -csv -folder C:\\scratchprojects\\sampleSolutions\\results.csv");
+    }
+
+    static Options getCommandLineOptions() {
+        Options options = new Options();
+
+        options.addOption(SOURCE_PATH_SHORT, SOURCE_PATH, true, "file path (required)");
+        options.addOption(TARGET_PATH_SHORT, TARGET_PATH, true, "folder path to sample solutions (required)");
+        options.addOption(CSV_PATH_SHORT, CSV_PATH, true, "path to sample csv file (required)");
+        options.addOption(HELP_SHORT, HELP, false, "print this message");
+
+        return options;
     }
 }
