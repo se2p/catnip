7c3928f215a73cd796d2a130f3aa54696c59a4aa
Florian Obermueller <obermuel@fim.uni-passau.de>
2020-10-06 18:56:46 +0200

    first draft of csv selector

--- src/main/java/de/uni_passau/fim/se2/catnip/Main.java
+++ src/main/java/de/uni_passau/fim/se2/catnip/Main.java
@@ -11,6 +11,8 @@ public class Main {
     private static final String CSV_PATH_SHORT = "c";
     private static final String HELP = "help";
     private static final String HELP_SHORT = "h";
+    private static final String MINIMUM_PERCENTAGE = "minpercentage";
+    private static final String MINIMUM_PERCENTAGE_SHORT = "m";
 
     private Main() {
     }
@@ -24,7 +26,6 @@ public class Main {
 
             if (cmd.hasOption(SOURCE_PATH) && cmd.hasOption(TARGET_PATH) && cmd.hasOption(CSV_PATH)) {
 
-                //TODO
 
                 return;
             }
@@ -39,7 +40,7 @@ public class Main {
         HelpFormatter formatter = new HelpFormatter();
         formatter.printHelp("Catnip", getCommandLineOptions(), true);
         System.out.println("Example: " + "java -jar Catnip.jar -path "
-                + "C:\\scratchprojects\\files\\mySolution.sb3 -target C:\\scratchprojects\\sampleSolutions -csv -folder C:\\scratchprojects\\sampleSolutions\\results.csv");
+                + "C:\\scratchprojects\\files\\mySolution.sb3 -target C:\\scratchprojects\\sampleSolutions -csv -folder C:\\scratchprojects\\sampleSolutions\\results.csv -m 80");
     }
 
     static Options getCommandLineOptions() {
@@ -48,6 +49,7 @@ public class Main {
         options.addOption(SOURCE_PATH_SHORT, SOURCE_PATH, true, "file path (required)");
         options.addOption(TARGET_PATH_SHORT, TARGET_PATH, true, "folder path to sample solutions (required)");
         options.addOption(CSV_PATH_SHORT, CSV_PATH, true, "path to sample csv file (required)");
+        options.addOption(MINIMUM_PERCENTAGE_SHORT, MINIMUM_PERCENTAGE, true, "minimum percentage of successful tests a target needs to be considered (default 90)");
         options.addOption(HELP_SHORT, HELP, false, "print this message");
 
         return options;
