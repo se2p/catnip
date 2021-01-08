825444fb9859c4460aa43a95f74071ac8cb8b877
Florian Obermueller <obermuel@fim.uni-passau.de>
2021-01-08 11:56:58 +0100

    address case where source has more actors than target

--- src/main/java/de/uni_passau/fim/se2/catnip/recommendation/EditsGenerator.java
+++ src/main/java/de/uni_passau/fim/se2/catnip/recommendation/EditsGenerator.java
@@ -39,110 +39,113 @@ public class EditsGenerator {
         }
 
         for (ActorWithProfile currentSourceActor : sourceActorDefinitionsWithProfile) {
-            ActorWithProfile currentTargetActor = NearestASTNodePicker.pickNearestActor(currentSourceActor,
-                    targetActorDefinitionsWithProfile);
+            //introduce edit that addresses deletion of actors
+            if (targetActorDefinitionsWithProfile.size() > 0) {
+                ActorWithProfile currentTargetActor = NearestASTNodePicker.pickNearestActor(currentSourceActor,
+                        targetActorDefinitionsWithProfile);
 
-            List<Script> sourceScripts = new ArrayList<>((
-                    (ActorDefinition) currentSourceActor.getASTNode()).getScripts().getScriptList());
-            List<Script> targetScripts =
-                    new ArrayList<>(((ActorDefinition) currentTargetActor.getASTNode()).getScripts().getScriptList());
+                List<Script> sourceScripts = new ArrayList<>((
+                        (ActorDefinition) currentSourceActor.getASTNode()).getScripts().getScriptList());
+                List<Script> targetScripts =
+                        new ArrayList<>(((ActorDefinition) currentTargetActor.getASTNode()).getScripts().getScriptList());
 
-            List<ScriptWithProfile> sourceScriptsWithProfile = new ArrayList<>();
-            for (Script script : sourceScripts) {
-                sourceScriptsWithProfile.add(new ScriptWithProfile(script));
-            }
-
-            List<ScriptWithProfile> targetScriptsWithProfile = new ArrayList<>();
-            for (Script script : targetScripts) {
-                targetScriptsWithProfile.add(new ScriptWithProfile(script));
-            }
-
-            if (targetScriptsWithProfile.size() < sourceScriptsWithProfile.size()) {
-                List<ScriptWithProfile> sourceScriptsNew = new ArrayList<>(sourceScriptsWithProfile);
-                for (ScriptWithProfile script : targetScriptsWithProfile) {
-                    ScriptWithProfile sourceScript = NearestASTNodePicker.pickNearestScript(script,
-                            sourceScriptsNew);
-                    sourceScriptsNew.remove(sourceScript);
+                List<ScriptWithProfile> sourceScriptsWithProfile = new ArrayList<>();
+                for (Script script : sourceScripts) {
+                    sourceScriptsWithProfile.add(new ScriptWithProfile(script));
                 }
-                assert sourceScriptsNew.size() > 0;
-                EditSet edit = new EditSet();
-                for (ScriptWithProfile script : sourceScriptsNew) {
-                    if (((Script) script.getASTNode()).getEvent() instanceof Never) {
-                        edit.addDeletion(new Edit(new Label("Script", null), new Label(
-                                ((Script) script.getASTNode()).getStmtList().getStmts().get(0).getClass().getSimpleName() + 0,
-                                ((Script) script.getASTNode()).getStmtList().getStmts().get(0))));
-                    } else {
-                        edit.addDeletion(new Edit(new Label("Script", null), new Label(
-                                ((Script) script.getASTNode()).getEvent().getClass().getSimpleName(),
-                                ((Script) script.getASTNode()).getEvent())));
+
+                List<ScriptWithProfile> targetScriptsWithProfile = new ArrayList<>();
+                for (Script script : targetScripts) {
+                    targetScriptsWithProfile.add(new ScriptWithProfile(script));
+                }
+
+                if (targetScriptsWithProfile.size() < sourceScriptsWithProfile.size()) {
+                    List<ScriptWithProfile> sourceScriptsNew = new ArrayList<>(sourceScriptsWithProfile);
+                    for (ScriptWithProfile script : targetScriptsWithProfile) {
+                        ScriptWithProfile sourceScript = NearestASTNodePicker.pickNearestScript(script,
+                                sourceScriptsNew);
+                        sourceScriptsNew.remove(sourceScript);
                     }
-                    edits.add(new ActorScriptEdit((ActorDefinition) currentSourceActor.getASTNode(),
-                            (Script) script.getASTNode(), edit));
-                }
-                sourceScriptsWithProfile.removeAll(sourceScriptsNew);
-            }
-
-            for (ScriptWithProfile sourceScript : sourceScriptsWithProfile) {
-
-                ScriptWithProfile targetScript = NearestASTNodePicker.pickNearestScript(sourceScript,
-                        targetScriptsWithProfile);
-                EditSet edit = PQGramUtil.identifyEdits(sourceScript.getProfile(),
-                        targetScript.getProfile());
-                if (edit.getAdditions().size() > 0 || edit.getDeletions().size() > 0) {
-                    edits.add(new ActorScriptEdit((ActorDefinition) currentSourceActor.getASTNode(),
-                            (Script) sourceScript.getASTNode(), edit));
-                }
-                targetScriptsWithProfile.remove(targetScript);
-            }
-
-            if (sourceScriptsWithProfile.size() < targetScriptsWithProfile.size()) {
-                EditSet edit = new EditSet();
-                for (ScriptWithProfile targetScript : targetScriptsWithProfile) {
-                    if (((Script) targetScript.getASTNode()).getEvent() instanceof Never) {
-                        edit.addAddition(new Edit(new Label("Script", null), new Label(
-                                ((Script) targetScript.getASTNode()).getStmtList().getStmts().get(0).getClass().getSimpleName() + 0,
-                                ((Script) targetScript.getASTNode()).getStmtList().getStmts().get(0))));
-                    } else {
-                        edit.addAddition(new Edit(new Label("Script", null), new Label(
-                                ((Script) targetScript.getASTNode()).getEvent().getClass().getSimpleName(),
-                                ((Script) targetScript.getASTNode()).getEvent())));
+                    assert sourceScriptsNew.size() > 0;
+                    EditSet edit = new EditSet();
+                    for (ScriptWithProfile script : sourceScriptsNew) {
+                        if (((Script) script.getASTNode()).getEvent() instanceof Never) {
+                            edit.addDeletion(new Edit(new Label("Script", null), new Label(
+                                    ((Script) script.getASTNode()).getStmtList().getStmts().get(0).getClass().getSimpleName() + 0,
+                                    ((Script) script.getASTNode()).getStmtList().getStmts().get(0))));
+                        } else {
+                            edit.addDeletion(new Edit(new Label("Script", null), new Label(
+                                    ((Script) script.getASTNode()).getEvent().getClass().getSimpleName(),
+                                    ((Script) script.getASTNode()).getEvent())));
+                        }
+                        edits.add(new ActorScriptEdit((ActorDefinition) currentSourceActor.getASTNode(),
+                                (Script) script.getASTNode(), edit));
                     }
+                    sourceScriptsWithProfile.removeAll(sourceScriptsNew);
                 }
-                edits.add(new ActorScriptEdit((ActorDefinition) currentSourceActor.getASTNode(), new EmptyScript(), edit));
-            }
 
-            List<ProcedureDefinition> sourceProcedures =
-                    new ArrayList<>(((ActorDefinition)
-                            currentSourceActor.getASTNode()).getProcedureDefinitionList().getList());
-            List<ProcedureDefinition> targetProcedures =
-                    new ArrayList<>(((ActorDefinition)
-                            currentTargetActor.getASTNode()).getProcedureDefinitionList().getList());
+                for (ScriptWithProfile sourceScript : sourceScriptsWithProfile) {
 
-            List<ProcedureWithProfile> sourceProceduresWithProfile =
-                    new ArrayList<>();
-            for (ProcedureDefinition procedure : sourceProcedures) {
-                sourceProceduresWithProfile.add(new ProcedureWithProfile(procedure));
-            }
-            List<ProcedureWithProfile> targetProceduresWithProfile =
-                    new ArrayList<>();
-            for (ProcedureDefinition procedure : targetProcedures) {
-                targetProceduresWithProfile.add(new ProcedureWithProfile(procedure));
-            }
-
-            for (ProcedureWithProfile sourceProcedure : sourceProceduresWithProfile) {
-                ProcedureWithProfile targetProcedure =
-                        NearestASTNodePicker.pickNearestProcedureDefinition(sourceProcedure,
-                                targetProceduresWithProfile);
-                EditSet edit = PQGramUtil.identifyEdits(sourceProcedure.getProfile(),
-                        targetProcedure.getProfile());
-                if (edit.getAdditions().size() > 0 || edit.getDeletions().size() > 0) {
-                    edits.add(new ActorProcedureEdit((ActorDefinition) currentSourceActor.getASTNode(),
-                            (ProcedureDefinition) sourceProcedure.getASTNode(), edit));
+                    ScriptWithProfile targetScript = NearestASTNodePicker.pickNearestScript(sourceScript,
+                            targetScriptsWithProfile);
+                    EditSet edit = PQGramUtil.identifyEdits(sourceScript.getProfile(),
+                            targetScript.getProfile());
+                    if (edit.getAdditions().size() > 0 || edit.getDeletions().size() > 0) {
+                        edits.add(new ActorScriptEdit((ActorDefinition) currentSourceActor.getASTNode(),
+                                (Script) sourceScript.getASTNode(), edit));
+                    }
+                    targetScriptsWithProfile.remove(targetScript);
                 }
-                targetProceduresWithProfile.remove(targetProcedure);
-            }
 
-            targetActorDefinitionsWithProfile.remove(currentTargetActor);
+                if (sourceScriptsWithProfile.size() < targetScriptsWithProfile.size()) {
+                    EditSet edit = new EditSet();
+                    for (ScriptWithProfile targetScript : targetScriptsWithProfile) {
+                        if (((Script) targetScript.getASTNode()).getEvent() instanceof Never) {
+                            edit.addAddition(new Edit(new Label("Script", null), new Label(
+                                    ((Script) targetScript.getASTNode()).getStmtList().getStmts().get(0).getClass().getSimpleName() + 0,
+                                    ((Script) targetScript.getASTNode()).getStmtList().getStmts().get(0))));
+                        } else {
+                            edit.addAddition(new Edit(new Label("Script", null), new Label(
+                                    ((Script) targetScript.getASTNode()).getEvent().getClass().getSimpleName(),
+                                    ((Script) targetScript.getASTNode()).getEvent())));
+                        }
+                    }
+                    edits.add(new ActorScriptEdit((ActorDefinition) currentSourceActor.getASTNode(), new EmptyScript(), edit));
+                }
+
+                List<ProcedureDefinition> sourceProcedures =
+                        new ArrayList<>(((ActorDefinition)
+                                currentSourceActor.getASTNode()).getProcedureDefinitionList().getList());
+                List<ProcedureDefinition> targetProcedures =
+                        new ArrayList<>(((ActorDefinition)
+                                currentTargetActor.getASTNode()).getProcedureDefinitionList().getList());
+
+                List<ProcedureWithProfile> sourceProceduresWithProfile =
+                        new ArrayList<>();
+                for (ProcedureDefinition procedure : sourceProcedures) {
+                    sourceProceduresWithProfile.add(new ProcedureWithProfile(procedure));
+                }
+                List<ProcedureWithProfile> targetProceduresWithProfile =
+                        new ArrayList<>();
+                for (ProcedureDefinition procedure : targetProcedures) {
+                    targetProceduresWithProfile.add(new ProcedureWithProfile(procedure));
+                }
+
+                for (ProcedureWithProfile sourceProcedure : sourceProceduresWithProfile) {
+                    ProcedureWithProfile targetProcedure =
+                            NearestASTNodePicker.pickNearestProcedureDefinition(sourceProcedure,
+                                    targetProceduresWithProfile);
+                    EditSet edit = PQGramUtil.identifyEdits(sourceProcedure.getProfile(),
+                            targetProcedure.getProfile());
+                    if (edit.getAdditions().size() > 0 || edit.getDeletions().size() > 0) {
+                        edits.add(new ActorProcedureEdit((ActorDefinition) currentSourceActor.getASTNode(),
+                                (ProcedureDefinition) sourceProcedure.getASTNode(), edit));
+                    }
+                    targetProceduresWithProfile.remove(targetProcedure);
+                }
+
+                targetActorDefinitionsWithProfile.remove(currentTargetActor);
+            }
         }
         return edits;
     }
