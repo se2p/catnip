7b0b53367df9285dcd78f4ae4ad9a397f2f0a666
Florian Obermueller <obermuel@fim.uni-passau.de>
2021-01-19 14:11:48 +0100

    introduce individual target selection

--- src/main/java/de/uni_passau/fim/se2/catnip/recommendation/TargetSelector.java
+++ src/main/java/de/uni_passau/fim/se2/catnip/recommendation/TargetSelector.java
@@ -20,7 +20,7 @@ public class TargetSelector {
         currentPercentage = DEFAULT_PERCENTAGE;
     }
 
-    public List<String> getViableTargetNames(String path, String sourceName) throws IOException, CsvException {
+    public List<String> getViableTargetNamesByPercentage(String path, String sourceName) throws IOException, CsvException {
         CSVReader reader = new CSVReader(new FileReader(path));
         List<String[]> myEntries = reader.readAll();
 
@@ -66,5 +66,60 @@ public class TargetSelector {
             }
         }
         return suitableProjects;
+    }
+
+    public List<String> getViableTargetNamesIndividualBetter(String path, String sourceName) throws IOException, CsvException {
+        CSVReader reader = new CSVReader(new FileReader(path));
+        List<String[]> myEntries = reader.readAll();
+
+        List<String> suitableProjects = new ArrayList<>();
+        boolean hasSkip = false;
+        String[] sourceEntry = null;
+        for (String[] currentEntry : myEntries) {
+            //format of csv after converting taps with taps to csv converter
+            if (currentEntry[0].equals("projektname") || currentEntry[0].equals("projectname")) {
+                if (currentEntry[currentEntry.length - 2].equals("skip")) {
+                    hasSkip = true;
+                }
+            } else if (currentEntry[0].equals(sourceName)) {
+                sourceEntry = currentEntry;
+                break;
+            }
+        }
+        if (sourceEntry == null) {
+            return getViableTargetNamesByPercentage(path, sourceName);
+        }
+        for (String[] currentEntry : myEntries) {
+            //format of csv after converting taps with taps to csv converter
+            if (!currentEntry[0].equals("projektname") && !currentEntry[0].equals("projectname") && !currentEntry[0].equals(sourceName)) {
+                if (isIndividuallyBetter(currentEntry, sourceEntry, hasSkip)) {
+                    suitableProjects.add(currentEntry[0]);
+                }
+            }
+        }
+        if (suitableProjects.size() == 0) {
+            return getViableTargetNamesByPercentage(path, sourceName);
+        }
+        return suitableProjects;
+    }
+
+    private boolean isIndividuallyBetter(String[] currentEntry, String[] sourceEntry, boolean hasSkip) {
+        int passedPosition;
+        if (hasSkip) {
+            passedPosition = currentEntry.length - 5;
+        } else {
+            passedPosition = currentEntry.length - 4;
+        }
+        int currentPassed = Integer.parseInt(currentEntry[passedPosition]);
+        int sourcePassed = Integer.parseInt(sourceEntry[passedPosition]);
+        if (currentPassed <= sourcePassed) {
+            return false;
+        }
+        for (int i = 1; i < passedPosition; i++) {
+            if (sourceEntry[i].equals("pass") && !currentEntry[i].equals("pass")) {
+                return false;
+            }
+        }
+        return true;
     }
 }
